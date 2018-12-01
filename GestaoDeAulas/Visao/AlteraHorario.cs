f5ed9f89cf94dcf89f68b401756f97a39b4ab0e2
Samuel <Samuel@DESKTOP-1QBR86I>
2018-11-30 22:10:25 -0200

    Projeto semi finalizado Falta correção de erros e relatórios

--- GestaoDeAulas/Visao/AlteraHorario.cs
+++ GestaoDeAulas/Visao/AlteraHorario.cs
@@ -1,12 +1,5 @@
 ﻿using GestaoDeAulas.Controle;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GestaoDeAulas.Visao
@@ -49,6 +42,11 @@ namespace GestaoDeAulas.Visao
         private void AlteraHorario_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
